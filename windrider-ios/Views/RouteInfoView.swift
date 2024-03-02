1793aa45fa3d8a2b78c236bab039aa8b5bdd77d2
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-03-02 21:27:44 +0100

    feat: added compute functions for coordinate directions

---
+++ windrider-ios/Views/RouteInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  RouteInfoView.swift
+//  windrider-ios
+//
+//  Created by Daniel Grbac Bravo on 02/03/2024.
+//
+
+import SwiftUI
+
+struct RouteInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RouteInfoView()
+}
