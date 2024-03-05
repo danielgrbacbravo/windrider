dffad798826fd18be70a6dfb1fc46019df1b6d7b
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-03-06 00:05:18 +0100

    refactor: refactored BikeRoute to comform with swiftDatta

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Daniel Grbac Bravo on 05/03/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
