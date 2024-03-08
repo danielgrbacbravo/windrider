3effd484e62714fe4b2789af1aff9bdc1ad7f4ba
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-03-08 11:47:06 +0100

    fix: bugs with modelContainer

---
+++ windrider/Model/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  windrider
+//
+//  Created by Daniel Grbac Bravo on 07/03/2024.
+//
+
+import Foundation
---
+++ windrider/RouteConditionPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  RouteConditionPreviewView.swift
+//  windrider
+//
+//  Created by Daniel Grbac Bravo on 07/03/2024.
+//
+
+import SwiftUI
+
+struct RouteConditionPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RouteConditionPreviewView()
+}
