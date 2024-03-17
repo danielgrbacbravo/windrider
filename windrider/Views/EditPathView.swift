cd57fe1740fbf1f4b08d97da650dabd34199f3eb
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-03-17 17:39:41 +0100

    message: added todo comments

---
+++ windrider/Views/EditPathView.swift
@@ -0,0 +1,15 @@
+//
+//  EditPathView.swift
+//  windrider
+//
+//  Created by Daniel Grbac Bravo on 17/03/2024.
+//
+
+import Foundation
+import  SwiftUI
+import SwiftData
+
+
+
+//TODO: Add a way to edit the path name
+//this will be apart of the RouteSelectionView and will be a button that will present a modal to edit the name of the path
