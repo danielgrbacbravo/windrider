28e626aad1c0682eed2b779c47419df332d1c146
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-03-17 14:15:39 +0100

    feat: added favorites to selection

--- windrider/Views/RouteSelectionView.swift
+++ windrider/Views/RouteSelectionView.swift
@@ -9,37 +9,46 @@ import SwiftUI
 import SwiftData
 
 struct RouteSelectionView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var paths: [CyclingPath]
-    @Binding var selectedPath: CyclingPath?
-    @Binding var isRouteSelectionViewPresented: Bool
-    
-    var body: some View {
-        NavigationStack{
-            
-            List(selection: $selectedPath) {
-                ForEach(paths, id: \.id) { route in
-                    Button(action: {
-                        selectedPath = route
-                        isRouteSelectionViewPresented = false
-                        
-                    }) {
-                        HStack{
-                            Text(route.name)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                        }
-                    }
-                }
-                .onDelete(perform: { indexSet in
-                    let index = indexSet.first!
-                    let route = paths[index]
-                    modelContext.delete(route)
-                  
-                })
-            }
-            .navigationTitle("Select Route")
-        }
-    }
+	@Environment(\.modelContext) private var modelContext
+	@Query private var paths: [CyclingPath]
+	@Binding var selectedPath: CyclingPath?
+	@Binding var isRouteSelectionViewPresented: Bool
+	
+	var body: some View {
+		NavigationStack{
+			List {
+				ForEach(paths, id: \.id) { route in
+					HStack{
+						Text(route.name).font(.headline).tint(.primary)
+						Spacer()
+						
+						if selectedPath == route {
+							Image(systemName:"checkmark").foregroundStyle(.green)
+						} else {
+							Image(systemName:"chevron.right").onTapGesture {
+								selectedPath = route
+								isRouteSelectionViewPresented = false
+							}
+						}
+						if route.isFavorite {
+							Image(systemName: "star.fill") .onTapGesture {
+								route.isFavorite = false
+							}.foregroundStyle(.yellow)
+						} else {
+							Image(systemName: "star").onTapGesture {
+								route.isFavorite = true
+							}.foregroundStyle(.yellow)
+						}
+					}
+				}
+				.onDelete(perform: { indexSet in
+					let index = indexSet.first!
+					let route = paths[index]
+					modelContext.delete(route)
+				})
+			}
+			.navigationTitle("Select Route")
+		}
+	}
+	
 }
-
